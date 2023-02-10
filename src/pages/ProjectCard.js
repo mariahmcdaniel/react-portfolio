6810fe0142af8ca1e6e4b699da39cddd2e6e793d
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-02-09 20:26:48 -0500

    projects

--- src/pages/ProjectCard.js
+++ src/pages/ProjectCard.js
@@ -1,26 +1,13 @@
 import React from 'react';
+import Portfolio from './Portfolio';
 
 export default function ProjectCard(props) {
   const cardStyle = {
     width: '18rem',
   };
-
-
-
   return (
     <div className="container">
-      <div className="card" style={cardStyle}>
-        <a href={props.link}>
-        <div className="card-body">
-        <h5 className="card-title">{props.name}</h5>
-          
-          <img
-          className="card-img"
-          src={props.img}
-          alt="code quiz"/>
-        </div>
-          </a>
-      </div>
+      <Portfolio />
     </div>
   );
 }
